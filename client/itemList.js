69cf1a8bbc3855c42064b95681e7c3fd2ba08fba
sebastianff <sebastianffarago@gmail.com>
2016-10-07 13:22:55 +0200

    Added database, removed autopublish, added a subscription

--- client/itemList.js
+++ client/itemList.js
@@ -1,15 +1,4 @@
-var postsData = [ {
-    title: 'Jeans',
-    url: 'http://ell.h-cdn.co/assets/16/11/480x720/elle-boyfriend-jeans-american-eagle-tomgirl.jpg'
-  },
-  {
-    title: 'Shirt',
-    url: 'http://www.customink.com/assets/site_content/products/220x225/unisex/color/blue-ca17c15034e90c68c7b6f72e91c20131b8239486eaf05389af9801799d4a8f34.jpg'
-}, {
-    title: 'Hat',
-    url: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRexwHIkcc_qkeb8MYnn960m-a_Le561nkzG3w6mcvfR7HEJ-wS'
-  }
-];
 Template.itemList.helpers({
-  posts: postsData
+  items: function() {
+    return Items.find(); }
 });
---
+++ server/fixtures.js
@@ -0,0 +1,12 @@
+if (Items.find().count() === 0) { Items.insert({
+    title: 'Jeans',
+    url: 'http://ell.h-cdn.co/assets/16/11/480x720/elle-boyfriend-jeans-american-eagle-tomgirl.jpg'
+	  });
+	  Items.insert({
+	    title: 'Shirt',
+	    url: 'http://www.customink.com/assets/site_content/products/220x225/unisex/color/blue-ca17c15034e90c68c7b6f72e91c20131b8239486eaf05389af9801799d4a8f34.jpg'
+	});
+	  Items.insert({
+	    title: 'Hat',
+	    url: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRexwHIkcc_qkeb8MYnn960m-a_Le561nkzG3w6mcvfR7HEJ-wS'
+	}); }
