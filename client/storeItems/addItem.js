2193a6b1f49941aa4e062659e31bf16bec298d87
sebastianff <sebastianffarago@gmail.com>
2016-10-08 22:46:48 +0200

    Create a method for adding new items, rearanged the file structure

--- client/storeItems/addItem.js
+++ client/storeItems/addItem.js
@@ -5,9 +5,12 @@ Template.addItem.events({
 			title: $(e.target).find('[id=itemName]').val(),
 			designer: $(e.target).find('[id=itemDesigner]').val(),
 			price: $(e.target).find('[id=itemPrice]').val(),
-			url: $(e.target).find('[id=itemPhoto]').val(),
+			url: $(e.target).find('[id=itemPhoto]').val()
 		};
-		Items.insert(item);
-	    	Router.go('itemList');
-	  	}
+		Meteor.call('addItem',item,function(error,result){
+		if(error)
+			return alert(error.reason);
+		Router.go('itemList');
+		})
+	}
 });
