571c1a431e579e007f2aca72c948a8aa2c454aa4
sebastianff <sebastianffarago@gmail.com>
2016-10-09 16:55:27 +0200

    Changed the look of the single product page

--- client/storeItems/itemList.js
+++ client/storeItems/itemList.js
@@ -1,5 +1,7 @@
 Template.itemList.helpers({
-  items: function() {
-    //return Items.find({designer:'Decko Car'}); }
-    return Items.find(); }
+  	items: function() {
+	    //return Items.find({designer:'Decko Car'}); }
+	    //return Items.find({},{sort:{submitted:-1}});
+	    return Items.find();//Above we have examples of filtering the items and sorting them
+	}
 });
